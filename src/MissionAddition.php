d170521c6fce4ad4ee7fd5d8bca868a6765710ed
missionphp@gmail.com <missionphp@gmail.com>
2018-05-22 10:55:51 -0700

    solved error

--- src/MissionAddition.php
+++ src/MissionAddition.php
@@ -6,9 +6,9 @@
 *  Use this section to define what this class is doing, the PHPDocumentator will use this
 *  to automatically generate an API documentation using this information.
 *
-*  @author yourname
+*  @author missionphp
 */
-class Addition{
+class MissionAddition{
 
   public function addMission($a,$b)
   {
