a3de5e0ea34d13437f3e656a332a4018d4383cbe
missionphp@gmail.com <missionphp@gmail.com>
2018-05-22 10:17:09 -0700

    first package

---
+++ src/MissionAddition.php
@@ -0,0 +1,17 @@
+<?php namespace Mission\Addition;
+
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author yourname
+*/
+class Addition{
+
+  public function addMission($a,$b)
+  {
+  	return 2*($a*$b);
+  }
+}
